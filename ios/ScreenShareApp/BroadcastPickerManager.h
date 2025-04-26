8fc586b5be157d35ad897eefcf585e4021b22448
BalwinderGureta <bsgureta@gmail.com>
2025-04-26 12:23:02 +0930

    second v5

---
+++ ios/ScreenShareApp/BroadcastPickerManager.h
@@ -0,0 +1,11 @@
+//
+//  BroadcastPickerManager.h
+//  ScreenShareApp
+//
+//  Created by Balwinder Singh on 26/4/2025.
+//
+
+#import <React/RCTViewManager.h>
+
+@interface BroadcastPickerManager : RCTViewManager
+@end
